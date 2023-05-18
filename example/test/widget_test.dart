0bc8564615f7f4e5eda032a86374cf7630ff70b2
Edi <poqueque@yahoo.es>
2023-05-18 07:37:43 +0200

    Added Web support to tests

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,26 +5,37 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:example/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('We can find all elements defined', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pumpWidget(App());
+    final richTextWidget =
+        tester.element(find.byType(RichText).first).widget as RichText;
+    final textSpan = richTextWidget.text as TextSpan;
+    expect(textSpan.children!.length, 16);
+    expect((textSpan.children![0] as TextSpan).text, "This is a ");
+    expect((textSpan.children![3] as TextSpan).text, "with a link");
+    expect(
+        (textSpan.children![3] as TextSpan).style!.fontStyle, FontStyle.normal);
+    expect((textSpan.children![3] as TextSpan).style!.decoration,
+        TextDecoration.underline);
+    expect((textSpan.children![5] as TextSpan).style!.decoration,
+        TextDecoration.underline);
+    expect((textSpan.children![7] as TextSpan).style!.decoration,
+        TextDecoration.underline);
+    expect((textSpan.children![8] as TextSpan).style!.decoration,
+        TextDecoration.underline);
+    expect((textSpan.children![4] as TextSpan).recognizer, null);
+    expect((textSpan.children![4] as TextSpan).recognizer, null);
+    expect((textSpan.children![8] as TextSpan).recognizer, isNot(null));
+    expect((textSpan.children![10] as TextSpan).style!.color,
+        Color.fromRGBO(255, 0, 0, 1));
+    expect((textSpan.children![12] as TextSpan).style!.color,
+        Colors.deepPurpleAccent);
+    expect((textSpan.children![14] as TextSpan).style!.color, Colors.green);
   });
 }

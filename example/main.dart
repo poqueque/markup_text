c7d1bfbdee2fc744602cf1b48bc9b0ff700e8fc4
Edi <poqueque@yahoo.es>
2023-05-18 06:59:27 +0200

    Fixed issue 2
    * Icon list updated to last stable version (Flutter 3.10.1)
    * Updated dependencies
    * Using Map instead of funcion to fix issue 2
    * Added macos to example app

--- example/main.dart
+++ example/main.dart
@@ -20,8 +20,8 @@ class App extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               MarkupText(
-                "This is a (b)bold(/b) text (a http://flutter.dev)with a link(/a),"
-                " an (u)underlined(/u) word (a http://pub.dev)with"
+                "This is a (b)bold(/b) text (a https://flutter.dev)with a link(/a),"
+                " an (u)underlined(/u) word (a https://pub.dev)with"
                 " a second link containing a word in (i)italics(/i)(/a) and (c #ff0000)colored(/c) words"
                 " (c deepPurpleAccent)here(/c) and (c green)there(/c).",
                 style: TextStyle(fontSize: 18),
